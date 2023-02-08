8c8e5b77abf76102c5ee6038839f30efa752eb08
IshaySela <ishaisela@gmail.com>
2023-02-08 19:13:00 +0200

    Added markdown support.
    Displaying compiled markdown, after sanitizing with DOMPurify

--- src/routes/content/[id]/+page.svelte
+++ src/routes/content/[id]/+page.svelte
@@ -2,19 +2,29 @@
   import { page } from "$app/stores";
   import { onMount } from "svelte";
   import type { Content } from "../../../Models/Content";
-  import { marked } from 'marked'
+  import { marked } from "marked";
+  import DOMPurify from 'dompurify'
   let contentId: string | undefined = undefined;
   let content: Content | undefined = undefined;
 
   onMount(() => {
     contentId = $page.params.id;
     const contentJson = localStorage.getItem(contentId);
-    if(contentJson === null) {
-      return window.location.href = '/';
+    if (contentJson === null) {
+      return (window.location.href = "/");
     }
 
     content = JSON.parse(contentJson) as Content;
   });
+
+  const getSanitizedHtml = () => {
+    if (content?.markdownContent === undefined) {
+      console.error('Cant compile, content is undefined')
+      return '';
+    }
+
+    return DOMPurify.sanitize(marked.parse(content.markdownContent))
+  };
 </script>
 
 {#if content !== undefined}
@@ -22,7 +32,7 @@
     <img src={content.bannerImage} alt="some alt" />
     <h1>{content.title}</h1>
     <div class="rendered-markdown">
-      {@html marked.parse(content.markdownContent)}
+      {@html getSanitizedHtml()}
     </div>
   </div>
 {:else}
