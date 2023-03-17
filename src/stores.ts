29121201dda1d6afa9cbb2bdf3e904eda2ede460
IshaySela <ishaisela@gmail.com>
2023-03-17 16:32:47 +0200

    Fixed an issue where notifications would be removed on page reload

--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 import type { AppNotification } from "./Models/Notification";
+import { persist, createLocalStorage } from "@macfja/svelte-persistent-store";
 
 
-export let notifications = writable<AppNotification>()
+export let notifications = persist(writable<AppNotification>(), createLocalStorage(), 'appNotificationStore')
