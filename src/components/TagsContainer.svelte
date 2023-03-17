9a1f7995ca29c0403914357a797884f2cb1d7c37
IshaySela <ishaisela@gmail.com>
2023-03-17 17:01:09 +0200

    Added smui package

--- src/components/TagsContainer.svelte
+++ src/components/TagsContainer.svelte
@@ -2,6 +2,7 @@
     The TagsContainer component is responsible for displaying tags.
 -->
 <script lang="ts">
+  import Chip, { Set, Text } from '@smui/chips'
   export let tags: string[];
 
   // Returns the bullet entity code, only if it is not the last tag.
@@ -9,12 +10,11 @@
     index !== tags.length - 1 ? "&bull;" : "";
 </script>
 
-<div class="tags-container">
-  {#each tags as tag, index}
-    <p>{tag}</p>
-    <p>{@html getBullEntity(index)}</p>
-  {/each}
-</div>
+<Set chips={tags} let:chip nonInteractive>
+  <Chip chip={chip}>
+    <Text>{chip}</Text>
+  </Chip>
+</Set>
 
 <style>
   .tags-container {
